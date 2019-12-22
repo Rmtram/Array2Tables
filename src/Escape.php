1f824e378e80d387752a911ef8958d4da47b7bee
Rmtram <rmtram95@gmail.com>
2019-12-22 16:10:47 +0900

    :sparkles: Add escape html

---
+++ src/Escape.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Rmtram\Array2Tables;
+
+/**
+ * Class Escape
+ * @package Rmtram\Array2Tables
+ */
+class Escape
+{
+    /**
+     * @param $string
+     * @return string
+     */
+    public static function h($string)
+    {
+        if (!is_string($string)) {
+            return $string;
+        }
+        return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+    }
+}
